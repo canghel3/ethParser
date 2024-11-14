9aefb78676d50c1a911f4717c3eec3672c31e395
cristian <cristian.anghel4@gmail.com>
2024-11-14 18:38:25 +0200

    reomve weird comment

--- storage/storage.go
+++ storage/storage.go
@@ -4,13 +4,12 @@ import (
 	"github.com/canghel3/ethereumBlockchainParser/models"
 )
 
-// Storage defines the functionality that any type of storage should implement
-// for storing address transactions.
 type Storage interface {
 	Create(address string) error
 	Read(address string) ([]models.Transaction, error)
 	Add(address string, transaction models.Transaction) error
 	Delete(address string) error
+
 	ReadAllTransactions() []models.Transaction
 	ReadAllSubscribers() []string
 }
